22ac589bb4eef97adcd69b069de77ff5c0ce2c56
Matthew Greenberg <greenberg.math@gmail.com>
2018-12-05 18:23:01 -0500

    Added more arm methods and also added Teleop Class (not sure if necessary)

---
+++ src/org/firstinspires/ftc/teamcode/Base/TeleOp.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Base;
+
+public class TeleOp extends OscarBaseOp {
+
+}
--- src/org/firstinspires/ftc/teamcode/Mechanisms/Arm.java
+++ src/org/firstinspires/ftc/teamcode/Mechanisms/Arm.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.Base.Hardware;
 import org.firstinspires.ftc.teamcode.Base.OscarCommon;
 
@@ -10,6 +11,17 @@ import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.*;
 public class Arm extends OscarCommon {
 
     private static DcMotor _intakeCollect, _intakeArmExtend, _intakeArmVertical;
+    private static Servo _dumpServo;
+
+    private static final double DUMP_OPEN = .7;
+    private static final int ARMYPOSITIONSCORE = 2750;
+    private static final int ARMEXTENDSCORE = 2800;
+    private static final int ARMYPOSITIONPARALLEL = 100;
+    private static final int ARMEXTENDPARALLEL = 1350;
+    private static final int ARMEXTENDMAX = 3000;
+    private static final int ARMVERTMAX = 2850;
+    private static final int ARMEXTENDMIN = 3000;
+    private static final int ARMVERTMIN = 2850;
 
     public static void init() {
         _intakeCollect = Hardware.MechanismMotors.intakeCollect;
@@ -33,18 +45,48 @@ public class Arm extends OscarCommon {
         _intakeArmExtend.setMode(RUN_TO_POSITION);
     }
 
-    public static int getArmVerticalPos() {
+    public static int getVerticalPos() {
         return _intakeArmVertical.getCurrentPosition();
     }
 
-    public static void moveArmVertical(int setpoint, double power) {
+    public static void moveVertical(int setpoint, double power) {
         _intakeArmVertical.setTargetPosition(setpoint);
         _intakeArmVertical.setPower(power);
     }
 
-    public static void moveArmHorizontal(int setpoint, double power) {
+    public static void moveHorizontal(int setpoint, double power) {
         _intakeArmExtend.setTargetPosition(setpoint);
         _intakeArmExtend.setPower(power);
+    }
+
+    public static void maxHeight() {
+        _intakeArmVertical.setTargetPosition(-ARMVERTMAX);
+        _intakeArmVertical.setPower(0.75);
+    }
+
+    public static void minHeight() {
+        _intakeArmVertical.setTargetPosition(-ARMVERTMIN);
+        _intakeArmVertical.setPower(0.5);
+    }
+
+    public static void extend() {
+        _intakeArmExtend.setTargetPosition(-ARMEXTENDMAX);
+        _intakeArmExtend.setPower(0.5);
+    }
+
+    public static void retract() {
+        _intakeArmExtend.setTargetPosition(-ARMEXTENDMIN);
+        _intakeArmExtend.setPower(0.5);
+    }
+
+    public static void score() {
+        maxHeight();
+        extend();
+    }
+
+    public static void ground() {
+        minHeight();
+        retract();
     }
 
     public static void succ(double succPower) {
@@ -56,6 +98,6 @@ public class Arm extends OscarCommon {
     }
 
     public static void dumpMineral() {
-
+        _dumpServo.setPosition(DUMP_OPEN);
     }
 }
