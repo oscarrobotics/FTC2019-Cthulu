80bbe4ab1c692cfcfb23de894faffacd1b6fe862
Matthew Greenberg <greenberg.math@gmail.com>
2018-12-04 22:29:09 -0500

    Added methods for lift positions

--- src/org/firstinspires/ftc/teamcode/Mechanisms/Lift.java
+++ src/org/firstinspires/ftc/teamcode/Mechanisms/Lift.java
@@ -7,6 +7,8 @@ import org.firstinspires.ftc.teamcode.Base.OscarCommon;
 
 public class Lift extends OscarCommon {
     private static DcMotor _elevator;
+    private static final int ELEVATOR_MAX = 4000;
+    private static final int ELEVATOR_MIN = 0;
 
     public static void init() {
         _elevator = Hardware.MechanismMotors.elevator;
@@ -17,5 +19,17 @@ public class Lift extends OscarCommon {
     public static void resetEncoders() {
         _elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         _elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public static void setPosition(int position){
+        _elevator.setTargetPosition(position);
+    }
+
+    public static void runToTop(){
+        setPosition(ELEVATOR_MAX);
+    }
+
+    public static void runToBottom(){
+        setPosition(ELEVATOR_MIN);
     }
 }
